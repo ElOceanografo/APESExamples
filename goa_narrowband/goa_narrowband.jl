7e77a93f36d84b0f760a2101a7734498c82de22e
Sam Urmy <oceanographerschoice@gmail.com>
2022-11-15 15:33:32 -0800

    finish goa narrowband

--- goa_narrowband/goa_narrowband.jl
+++ goa_narrowband/goa_narrowband.jl
@@ -1,6 +1,7 @@
 using Distributed
 addprocs(8, topology=:master_worker, exeflags="--project=$(Base.active_project())")
 using CSV
+using StatsBase
 @everywhere begin
     using ProbabilisticEchoInversion
     using Turing
@@ -23,7 +24,6 @@ end
     @dim D XDim "Distance (km)"
 end
 
-
 dfs = map(freqs) do f
     echo = CSV.read(joinpath(@__DIR__, "data/DY2104_T6_shelfbreak_$(f)_kHz.csv"),
         DataFrame)
@@ -43,11 +43,11 @@ dfs = map(freqs) do f
 end
 echodata = vcat(dfs...)
 allowmissing!(echodata)
+noise_floor = -120
+echodata.Sv[echodata.Sv .< noise_floor] .= noise_floor
 echodata[(echodata.Sv .> 0), [:sv, :Sv]] .= missing
 replace!(echodata.Sv, -Inf => missing)
-echodata[(echodata.freq .== 70) .& (echodata.depth .> 475), [:sv, :Sv]] .= missing
-echodata[(echodata.freq .== 120) .& (echodata.depth .> 350), [:sv, :Sv]] .= missing
-echodata[(echodata.freq .== 200) .& (echodata.depth .> 250), [:sv, :Sv]] .= missing
+
 echodata.distance = (echodata.distance .- minimum(echodata.distance)) / 1e3
 echodata = @subset(echodata,  5 .< :depth .< 400, :distance .< 30)
 depths = disallowmissing(sort(unique(echodata.depth)))
@@ -64,8 +64,26 @@ Sv = map(freqs) do f
     end
 end
 Sv = DimArray(cat(Sv..., dims=3), (Z(depths), D(distances), F(freqs)))
-sv = 10 .^ (Sv ./ 10)
-heatmap(Sv[F(At(38))], yflip=true, clim=(-90, -60))
+sv = exp10.(Sv ./ 10)
+
+bottom = @chain dfs[1] begin
+    @subset(5 .< :depth .< 400)
+    unstack(:depth, :distance, :Sv)
+    @orderby(:depth)
+    select(Not(:depth))
+    Array()
+    DimArray((Z(depths), D(distances)))
+end
+bottom .*= 0
+bottom[ismissing.(bottom)] .= 1
+b = [RGBA(0.5, 0.5, 0.5, x) for x in bottom]
+
+heatmap(Sv[F(At(70))], yflip=true, background_color_inside="#333333")
+plot!(distances, depths, b, yflip=true, aspect_ratio=0.03, xlims=(0, 18), ylims=(5, 395))
+
+annotation_x = [16,  5,   9,    12,  10]
+annotation_y = [40,  30,  90,  220, 290]
+annotation_text = text.(["A", "B", "C",  "D", "E"], color=:white, 12)
 
 sv_plots = map(freqs) do f
     if f == 200
@@ -76,8 +94,11 @@ sv_plots = map(freqs) do f
         xlabel=""
     end
     
-    heatmap(Sv[F(At(f))], yflip=true, clim=(-90, -60), title="", xticks=xticks, xlabel=xlabel,
+    p = heatmap(Sv[F(At(f))], yflip=true, title="", xticks=xticks, xlabel=xlabel, clim=(-90, -60),
+        # background_color_inside="#333333",
         topmargin=0px, bottommargin=0px, colorbar_title="Sᵥ ($(f) kHz)") 
+    annotate!(p, annotation_x, annotation_y, annotation_text)
+    plot!(distances, depths, b, yflip=true, aspect_ratio=0.01, xlims=(0, 18), ylims=(10, 400))
 end
 plot(sv_plots..., layout=(5, 1), size=(800, 900), leftmargin=20px)
 savefig(joinpath(@__DIR__, "plots/echograms.png"))
@@ -85,86 +106,107 @@ savefig(joinpath(@__DIR__, "plots/echograms.png"))
 @everywhere begin
 
     @model function echomodel(data, params)
-        cv ~ Exponential(0.1)
-        a ~ Uniform(1e-5, 1e-2)#truncated(Normal(2e-3, 4e-3), 0, 0.21)
-        δ = 0.3
+        ϵ ~ Exponential(0.5)
+        a ~ Uniform(1e-5, 2e-3)
+        δ = 0.5
         bubble = Bubble(a, depth=data.coords[1], δ=δ)
         TS_bubble = [target_strength(bubble, f*1e3) for f in freqs]
         TS = [params.TS TS_bubble]
         Σ = exp10.(TS ./ 10)
         logn ~ arraydist(params.prior)
         n = exp10.(logn)
-        sv_pred = Σ * n
-        ηsv = max.(cv * data.backscatter, sqrt(eps()))
+        Sv_pred = 10log10.(Σ * n)
 
         for i in eachindex(data.freqs)
             if ! ismissing(data.backscatter[i])
-                data.backscatter[i] ~ Normal(sv_pred[i], ηsv[i])
+                data.backscatter[i] ~ censored(Normal(Sv_pred[i], ϵ), params.noise_floor, Inf)
             end
         end
 
-        return 10log10.(sv_pred)
+        return Sv_pred
     end
 
 end
 
 krill = resize(Models.krill_mcgeehee, 0.025)
-# estimates from Lucca et al. 2021
+# values from Lucca et al. 2021
 krill.g .= 1.019
 krill.h .= 1.032
 TS_krill = [target_strength(krill, f*1e3, 1468.0) for f in freqs]
 TS_fish = [-34.6, -35.0, -35.6, -36.6, -38.5]
 TS_myctophid = [-73.0, -58.0, -65, -67.2, -70.0]
 
+labels = ["Large fish" "Myctophids" "Krill" "Bubble"]
 pars = (TS =    [TS_fish TS_myctophid TS_krill], 
         prior = [Normal(-3, 3),  # big fish
                  Normal(-2, 3),  # myctophid
                  Normal(-2, 3),  # krill
-                 Normal(-2, 3)]) # small bubble-like
+                 Normal(-2, 3)], # small bubble-like
+        noise_floor = noise_floor)
 
 solver_map = MAPSolver(optimizer=SimulatedAnnealing(), verbose=false)
-
-solution_map = apes(sv, echomodel, solver_map, params=pars, distributed=true)
+solution_map = apes(Sv, echomodel, solver_map, params=pars, distributed=true)
 
 fmissing(x, f) = ismissing(x) ? missing : f(x)
 
-heatmap(fmissing.(solution_map, x -> coef(x)[Symbol("logn[4]")]), yflip=true)
+heatmap(fmissing.(solution_map, x -> coef(x)[:a]), yflip=true, clims=(0, 0.0007))
+heatmap(fmissing.(solution_map, x -> coef(x)[Symbol("logn[2]")]), yflip=true,
+    clims=(-10, 0))
+
+solver_mcmc = MCMCSolver(verbose=false, nsamples=1000, nchains=4)
+solution_mcmc = apes(Sv, echomodel, solver_mcmc, params=pars, distributed=true)
+
+thresh_mask = fmissing.(solution_mcmc, chn -> mean(chn[Symbol("logn[4]")]) .> -6 ? 1.0 : missing)
+p1 = heatmap(fmissing.(solution_mcmc, chn -> mean(1e3chn[:a])) .* thresh_mask, yflip=true, 
+    c=cgrad(:hawaii, rev=true), clims=(0.2, 1.2), 
+    colorbar_title="Bubble radius (mm)", size=(600, 350))
+    plot!(p1, distances, depths, b, yflip=true, aspect_ratio=0.04, xlims=(0, 18), ylims=(5, 400))
+annotate!(p1, annotation_x, annotation_y, annotation_text)
+
+p2 = heatmap(fmissing.(solution_mcmc, chn -> std(1e3chn[:a])) .* thresh_mask, yflip=true, c=:viridis,
+    colorbar_title="Bubble radius C.V.")
+plot!(p2, distances, depths, b, yflip=true, aspect_ratio=0.04, xlims=(0, 18), ylims=(5, 400))
+annotate!(p2, annotation_x, annotation_y, annotation_text)
+plot(p1, p2, size=(1000, 350), margin=15px)
+savefig(joinpath(@__DIR__, "plots/bubble_esr.png"))
 
 
-solver_mcmc = MCMCSolver(verbose=false)
-solution_mcmc = apes(sv, echomodel, solver_mcmc, params=pars, distributed=true)
+meanplots = map(1:4) do i
+    μ = fmissing.(solution_mcmc, chn -> mean(chn[Symbol("logn[$(i)]")]))
+    ul = quantile(skipmissing(vec(μ)), 0.999)
+    ll = ul - 4
+    cl = (ll, ul)
+    xticks = i == 4 ? true : false
+    xlabel = i == 4 ? "Distance (km)" : ""
+    p = heatmap(μ, colorbartitle="log₁₀($(labels[i]) m⁻³)", yflip=true,
+        xlabel=xlabel, xticks=xticks, clims=cl, background_color_inside="#888888")
+    annotate!(p, annotation_x, annotation_y, annotation_text)
+    return p
+end
 
-threshold_mask = mapspectra(x -> all(x.backscatter .> -90), Sv)
-
-heatmap(fmissing.(solution_mcmc, chn -> median(1e3chn[:a])), yflip=true)
-heatmap(fmissing.(solution_mcmc, chn -> std(1e3chn[:a])), yflip=true, c=:viridis, clims=())
-# heatmap(fmissing.(solution_mcmc, chn -> mean(chn[:δ])), yflip=true)
-
+logcv(x) = std(exp10.(x)) / mean(exp10.(x))
+cvplots = map(1:4) do i
+    cv = fmissing.(solution_mcmc, chn -> logcv(chn[Symbol("logn[$(i)]")]))
+    cl = tuple(quantile(skipmissing(vec(cv)), [0.001, 0.999])...)
+    xticks = i == 4 ? true : false
+    xlabel = i == 4 ? "Distance (km)" : ""
+    p = heatmap(cv, colorbartitle="$(labels[i]) C.V.", yflip=true,
+        xlabel=xlabel, xticks=xticks, c=:viridis, clims=(0, 15), background_color_inside="#888888")
+    annotate!(p, annotation_x, annotation_y, annotation_text)
+    return p
+end
 plot(
-    heatmap(fmissing.(solution_mcmc, chn -> median(chn[Symbol("logn[1]")])), colorbartitle="log₁₀(large fish m⁻³)",
-        xlabel="", xticks=false, clims=(-6, -3)),
-    heatmap(fmissing.(solution_mcmc, chn -> median(chn[Symbol("logn[2]")])), colorbartitle="log₁₀(myctophid m⁻³)",
-        xlabel="", xticks=false, clims=(-4, -1)),
-    heatmap(fmissing.(solution_mcmc, chn -> median(chn[Symbol("logn[3]")])), colorbartitle="log₁₀(krill m⁻³)",
-        xlabel="", xticks=false, clims=(-4, -1)),
-    heatmap(fmissing.(solution_mcmc, chn -> median(chn[Symbol("logn[4]")])), colorbartitle="log₁₀(small bubble m⁻³)",
-        clim=(-7, -6)),
-    yflip=true, layout=(4, 1), size=(600, 600), 
-)
+    plot(meanplots..., layout=(4, 1)),
+    plot(cvplots..., layout=(4, 1)),
+    layout=(1, 2), size=(1200, 1000), margin=15px
+) 
+savefig(joinpath(@__DIR__, "plots/posteriors.png"))
 
+heatmap(fmissing.(solution_mcmc, chn -> mean(chn[:ϵ])), yflip=true, c=:viridis)
+savefig(joinpath(@__DIR__, "plots/epsilon.png"))
+heatmap(fmissing.(solution_mcmc, chn -> mean(chn[:lp])), yflip=true, c=:viridis)
+savefig(joinpath(@__DIR__, "plots/mean_log_probability.png"))
 
-plot(
-    heatmap(fmissing.(solution_mcmc, chn -> std(chn[Symbol("logn[1]")])), colorbartitle="log₁₀(large fish m⁻³)",
-        xlabel="", xticks=false, c=:viridis),
-    heatmap(fmissing.(solution_mcmc, chn -> std(chn[Symbol("logn[2]")])), colorbartitle="log₁₀(myctophid m⁻³)",
-        xlabel="", xticks=false, c=:viridis),
-    heatmap(fmissing.(solution_mcmc, chn -> std(chn[Symbol("logn[3]")])), colorbartitle="log₁₀(krill m⁻³)",
-        xlabel="", xticks=false, c=:viridis),
-    heatmap(fmissing.(solution_mcmc, chn -> std(chn[Symbol("logn[4]")])), colorbartitle="log₁₀(small bubble m⁻³)",
-        c=:viridis),
-    yflip=true, layout=(4, 1), size=(600, 600),
-)
-
-
-heatmap(fmissing.(solution_mcmc, chn -> mean(chn[:cv])), yflip=true, c=:viridis, clims=(0.11, 0.93))
-heatmap(fmissing.(solution_mcmc, chn -> mean(chn[:lp])), yflip=true, c=:viridis, clims=(50, 70))
+rhats = fmissing.(solution_mcmc, chn -> maximum(abs.(DataFrame(ess_rhat(chn)).rhat .- 1)))
+heatmap(rhats, yflip=true, clims=(0, 0.1), c=:viridis)
+savefig(joinpath(@__DIR__, "plots/max_rhat.png"))
